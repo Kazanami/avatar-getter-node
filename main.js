633c4a0f5525b1e4fe6252ba9701c421c80392c0
f0rk <dummy.github.1945@gmail.com>
2020-07-10 12:53:23 +0900

    programed

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 const { Canvas } = require("canvas-constructor");
 const { resolve, join } = require("path");
 const fetch = require("node-fetch");
+const fs = require("fs-extra")
+
+const imageUrl = `https://github.com/${process.env.GH_USER}.png?size=${process.env.IMAGE_SIZE}`
+
+async function downloadAvatar(url) {
+  console.log(`Downloading ${process.env.GH_USER}.png ...`);
+  const _userAvater = await fetch(url);
+  if (!_userAvater.ok){
+    throw new Error("Faild Image Download");
+  }
+  let avater_buffer = await _userAvater.buffer();
+  let created_image = createCircleImage(avater_buffer);
+  fs.writeFileSync(`./${process.env.GH_USER}.png`, created_image)
+}
+
+function createCircleImage(avater){
+  return new Canvas(256, 256)
+    .addCircle(84, 90, 62)
+    .addCircularImage(avater, 128,128,128)
+    .save()
+    .toBuffer()
+}
+
+downloadAvatar(imageUrl);
